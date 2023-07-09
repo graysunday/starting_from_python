d2a00fa00cc527ae8ccec72bd99f4a745a6dbb3a
graysunday <graysunday77@gmail.com>
2023-07-09 12:32:44 +0900

    added images

---
+++ fly.py
@@ -0,0 +1,6 @@
+import pygame, sys, time, random, math
+from pygame.locals import *
+pygame.init()
+clock = pygame.time.Clock()
+pygame.display.set_caption("Fly Catcher")
+screen = pygame.display.set_mode((1000,600))
